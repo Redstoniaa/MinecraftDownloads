8ed2029821e87d3f9f6b530a3268606058b6e9fd
Redstonia <redstonia1234@gmail.com>
2022-03-16 10:45:58 +1300

    a website has appeared

---
+++ docs/collapsible.js
@@ -0,0 +1,13 @@
+﻿function togglecollapsible(button) {
+    var content = button.parentElement.getElementsByClassName("collapsible-content")[0];
+
+    console.log(content.style.display);
+    console.log(button.parentElement.getElementsByClassName("collapsible-content")[0].style.display);
+    if (content.style.display == "block") {
+        content.style.display = "none";
+    }
+
+    else {
+        content.style.display = "block";
+    }
+}
