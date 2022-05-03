5ffc0fab6d5fab33acc7baa9b17e7895193be575
Redstonia <redstonia1234@gmail.com>
2022-05-03 17:37:06 +1200

    - added arrows to collapsibles, to show that you can expand them and that they're not just random grey boxes - changed a lot of css measurements to be in "px" rather than "em" - cursor now turns into pointer when hovering on collapsibles

--- docs/collapsible.js
+++ docs/collapsible.js
@@ -3,7 +3,19 @@
 
     console.log(content.style.display);
     console.log(button.parentElement.getElementsByClassName("collapsible-content")[0].style.display);
+    console.log(button.getElementsByClassName("arrow-container"));
 
-    if (content.style.display == "block") content.style.display = "none";
-    else content.style.display = "block";
+    if (content.style.display == "block") {
+        content.style.display = "none";
+
+        var arrow = button.getElementsByClassName("arrow")[0];
+        if (arrow) arrow.style.transform = "rotate(0deg)";
+    }
+
+    else {
+        content.style.display = "block";
+
+        var arrow = button.getElementsByClassName("arrow")[0];
+        if (arrow) arrow.style.transform = "rotate(-90deg)";
+    }
 }
